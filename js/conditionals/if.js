bf0bbf1483ec0daa1d9afd1a689c8fdbd550b171
Martin Rodriguez <neomartinr@gmail.com>
2022-09-05 19:29:26 -0300

    Else if

--- js/conditionals/if.js
+++ js/conditionals/if.js
@@ -11,21 +11,19 @@ console.log(edad)
 //2- Luego evaluar si su edad es de 12 años
 if( edad <= 12  ) {
 
-
     // console.info('Puede pasar')
     //Si la condición se cumple 
-    console.log('Si puede ingresar al pelotero')
-
-
-
+    console.log('Si puede ingresar al pelotero gratis')
 
 } 
-else {
+else if(edad <= 20) {
 
-    console.warn('No puede ingresar');
-    // console.warn('No puede ingresar');
+    console.log(`Puede entrar pero debe abonar $200`)
 
-
+} else {
+    console.warn('No puede ingresar al pelotero')
 }
+
+console.log(`Termino el script`)
     //a- Lo dejo entrar al pelotero
     //b- Le digo que no puede ingresar
