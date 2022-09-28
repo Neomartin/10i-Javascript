09fe0bbece7d330452478d09910d1d41cf75f083
Martin Rodriguez <neomartinr@gmail.com>
2022-09-28 19:14:06 -0300

    Arrays

---
+++ js/arrays/arrays-methods.js
@@ -0,0 +1,124 @@
+const usersTemp = [
+  {
+      "id": 11232,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "age": 34,
+      "email": "Sincere@april.biz",
+      "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556"
+      },
+      "active": true
+  },
+  {
+      "id": 25679,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "age": 23,
+      "email": "Shanna@melissa.tv",
+      "address": {
+          "street": "Victor Plains",
+          "suite": "Suite 879"
+      },
+      "active": true
+  },
+  {
+      "id": 31231,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+      "age": 28,
+      "active": false,
+      "address": {
+          "street": "Douglas Extension",
+          "suite": "Suite 847"
+      }
+  },
+  {
+      "id": 43232,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "email": "Julianne.OConner@kory.org",
+      "age": 32,
+      "active": false,
+      "address": {
+          "street": "Hoeger Mall",
+          "suite": "Apt. 692"
+      }
+  },
+  {
+      "id": 56778,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "email": "Lucio_Hettinger@annie.ca",
+      "age": 26,
+      "active": true,
+      "address": {
+          "street": "Skiles Walks",
+          "suite": "Suite 351"
+      }
+  },
+  {
+      "id": 66980,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "email": "Karley_Dach@jasper.info",
+      "age": 20,
+      "active": true,
+      "address": {
+          "street": "Norberto Crossing",
+          "suite": "Apt. 950"
+      }
+  },
+  {
+      "id": 74213,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "email": "Telly.Hoeger@billy.biz",
+      "age": 21,
+      "active": false,
+      "address": {
+          "street": "Rex Trail",
+          "suite": "Suite 280"
+      }
+  },
+  {
+      "id": 85614,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "email": "Sherwood@rosamond.me",
+      "age": 33,
+      "active": true,
+      "address": {
+          "street": "Ellsworth Summit",
+          "suite": "Suite 729"
+      }
+  },
+  {
+      "id": 94125,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "email": "Chaim_McDermott@dana.io",
+      "age": 25,
+      "active": true,
+      "address": {
+          "street": "Dayna Park",
+          "suite": "Suite 449"
+      }
+  },
+  {
+      "id": 10321,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "email": "Rey.Padberg@karina.biz",
+      "age": 32,
+      "active": true,
+      "address": {
+          "street": "Kattie Turnpike",
+          "suite": "Suite 198"
+      }
+  }
+];
+const users =  JSON.parse(JSON.stringify(usersTemp));
+
